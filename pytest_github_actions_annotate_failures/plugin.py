5c73f05c57675c27158af7a690ec1c44bf0216ad
utgwkk <utagawakiki@gmail.com>
2020-05-09 01:41:33 +0900

    annotate failed test functions

---
+++ pytest_github_actions_annotate_failures/plugin.py
@@ -0,0 +1,30 @@
+def pytest_runtest_logreport(report):
+    if report.outcome != 'failed':
+        return
+
+    # collect information to be annotated
+    filesystempath, lineno, _ = report.location
+
+    # 0-index to 1-index
+    lineno += 1
+
+    longrepr = str(report.longrepr)
+
+    print(_error_workflow_command(filesystempath, lineno, longrepr))
+
+def _error_workflow_command(filesystempath, lineno, longrepr):
+    if lineno is None:
+        if longrepr is None:
+            return '\n::error file={}'.format(filesystempath)
+        else:
+            longrepr = _escape(longrepr)
+            return '\n::error file={}::{}'.format(filesystempath, longrepr)
+    else:
+        if longrepr is None:
+            return '\n::error file={},lineno={}'.format(filesystempath, lineno)
+        else:
+            longrepr = _escape(longrepr)
+            return '\n::error file={},lineno={}::{}'.format(filesystempath, lineno, longrepr)
+
+def _escape(s: str):
+    return s.replace('%', '%25').replace('\r', '%0D').replace('\n', '%0A')
