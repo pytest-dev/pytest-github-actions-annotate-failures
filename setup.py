e1e2d4cdf9852a72479aebb6acbc0989587c6d37
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2023-01-07 15:35:18 +0000

    Require python 3.7+ (#66)

--- setup.py
+++ setup.py
@@ -15,13 +15,17 @@ setup(
     author_email="utagawakiki@gmail.com",
     url="https://github.com/pytest-dev/pytest-github-actions-annotate-failures",
     license="MIT",
-    classifiers=["Framework :: Pytest",],
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    classifiers=[
+        "Framework :: Pytest",
+    ],
+    python_requires=">=3.7",
     packages=find_packages(),
     entry_points={
         "pytest11": [
             "pytest_github_actions_annotate_failures = pytest_github_actions_annotate_failures.plugin",
         ],
     },
-    install_requires=["pytest>=4.0.0",],
+    install_requires=[
+        "pytest>=4.0.0",
+    ],
 )
